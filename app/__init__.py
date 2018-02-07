632db25b8a96d868df47ef5df945217b8cf87b25
Sam Ngigi <sam.t.ngigi@gmail.com>
2018-02-07 17:56:27 +0300

    added background color

--- app/__init__.py
+++ app/__init__.py
@@ -3,13 +3,16 @@ from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_uploads import UploadSet, configure_uploads, IMAGES
+from flask_mail import Mail
+from flask_simplemde import SimpleMDE
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 db = SQLAlchemy()
+mail = Mail()
 photos = UploadSet('photos', IMAGES)
-
+simple = SimpleMDE()
 # Initializing app
 
 
