97577b7c78a9958ef7400038771a0a56184d0d1e
Sam Ngigi <sam.t.ngigi@gmail.com>
2018-02-11 01:18:51 +0300

    Added posts page and added styling

--- app/main/views.py
+++ app/main/views.py
@@ -7,3 +7,9 @@ from flask import render_template
 def index():
     test = 'Working!'
     return render_template('index.html', test=test)
+
+
+@main.route('/posts')
+def posts():
+    test = 'Working'
+    return render_template('posts.html', test=test)
